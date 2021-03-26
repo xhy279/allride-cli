55f1520378895f28301c6c74139d407d38b7a687
jeremy <huanyu.xia@allride.ai>
2021-03-26 20:34:33 +0800

    inquirer implementation

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -20,13 +20,28 @@ class InitCommand extends Command {
 	async exec() {
 		try {
 			// 1. 准备阶段
-			await this.prepare();
+			const projectInfo = await this.prepare();
 			// 2. 下载模板
+      if (projectInfo) {
+        console.log(projectInfo);
+      }
 			// 3. 安装模板
 		} catch (e) {
+      console.log(e)
 			log.error(e.message);
 		}
 	}
+
+  /**
+   * 通过项目模板api获取项目模板信息
+   * 1. 通过egg.js搭建一套后端系统
+   * 2. 通过npm存储项目模板
+   * 3. 将项目模板存储到mongodb中
+   * 4. 通过egg.js获取mongodb中的数据并通过api返回
+   */
+  downloadTemplate() {
+
+  }
 
 	async prepare() {
 		const localPath = process.cwd();
@@ -46,12 +61,10 @@ class InitCommand extends Command {
 					})
 				).ifContinue;
 			}
-
 			// 用户选择不继续创建
 			if (!ifContinue) {
 				return;
 			}
-
 			// 如果强制创建
 			if (ifContinue || this.force) {
 				// 给用户做二次确认
@@ -72,7 +85,7 @@ class InitCommand extends Command {
 	}
 
 	async getProjectInfo() {
-		const projectInfo = {};
+		let projectInfo = {};
 		// 3. 选择项目或者组件
 		const { type } = await inquirer.prompt({
 			type: 'list',
@@ -88,37 +101,59 @@ class InitCommand extends Command {
 
 		// 4. 获取项目基本信息  => return 项目基本信息(object)
 		if (type === TYPE_PROJECT) {
-			const o = await inquirer.prompt([
+			const project = await inquirer.prompt([
 				{
 					type: 'input',
 					name: 'projectName',
 					message: '请输入项目名称',
 					default: '',
-					validate: function(v) {
-            // 首字符为英文字母，字符仅允许'-_', 末尾为数字和英文字母
-						return /^[a-zA-Z]+([-][a-zA-Z][a-zA-Z0-9]*|[_][a-zA-Z][a-zA-Z0-9]*|[a-zA-Z0-9])*$/.test(v);
+					validate: function (v) {
+						const done = this.async();
+						setTimeout(function () {
+							const valid = /^[a-zA-Z]+([-][a-zA-Z0-9]*|[_][a-zA-Z0-9]*|[a-zA-Z0-9])*$/.test(
+								v
+							);
+							if (!valid) {
+								done(
+									'需要提供有效的项目名称(首字符为英文字母，字符仅允许下划线和连字符, 末尾为数字或英文字母)'
+								);
+								return;
+							}
+							done(null, true);
+						}, 0);
 					},
-					filter: function(v) {
+					filter: function (v) {
 						return v;
 					},
 				},
 				{
-          type: 'input',
-          name: 'projectVersion',
-          message: '请输入项目版本号',
-          default: '',
-          validate: function (v) {
-            return !!semver.valid(v);
-          },
-          filter: function (v) {
-            return !!semver.valid(v) ? semver.valid(v) : v;
-          }
-        },
+					type: 'input',
+					name: 'projectVersion',
+					message: '请输入项目版本号',
+					default: '',
+					validate: function (v) {
+						const done = this.async();
+						setTimeout(function () {
+							const valid = !!semver.valid(v);
+							if (!valid) {
+								done('请输入合法的版本号(eg: 1.1.1 or v1.1.0)');
+								return;
+							}
+							done(null, true);
+						}, 0);
+					},
+					filter: function (v) {
+						return !!semver.valid(v) ? semver.valid(v) : v;
+					},
+				},
 			]);
-      console.log(o);
+      projectInfo = {
+        type,
+        ...project,
+      };
 		} else if (type === TYPE_COMPONENT) {
 		}
-
+		
 		return projectInfo;
 	}
 
